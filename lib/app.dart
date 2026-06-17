2c3e9ac6909e78cadc38e8e93aeecfd9213d0088
YoucefMebAmaneEllahGherbal <gherbalyoucef@gmail.com>
2026-06-17 17:05:25 +0100

    constant colors

---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'utils/theme/theme.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      theme: TAppTheme.LightTheme,
+      darkTheme: TAppTheme.DarkTheme,
+    );
+  }
+}
