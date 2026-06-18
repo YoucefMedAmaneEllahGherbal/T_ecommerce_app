ba865ab26d0c67a036e618226b75a36c011c2e1c
YoucefMebAmaneEllahGherbal <gherbalyoucef@gmail.com>
2026-06-18 11:53:11 +0100

    added text strings for titles subtitle and appbar

---
+++ lib/utils/constants/texts.dart
@@ -0,0 +1,15 @@
+class Ttexts {
+  static const String onBoardingTitle1 = "Choose Your Product";
+  static const String onBoardingTitle2 = "Select Payment Method";
+  static const String onBoardingTitle3 = "Deliver At Your Door Step";
+
+  static const String onBoardingSubitle1 =
+      "Welcome Tp A World Of Limitless Choices";
+  static const String onBoardingSubitle2 =
+      "For Seamles Transactions, Choose The payment Method That Suits";
+  static const String onBoardingSubitle3 =
+      "From Our Doorstep To Yours - Swift , Secure and Contactless Delivery";
+
+  static const String homeAppBarTitle = "Good day for Shopping";
+  static const String homeAppBarSubTitle = "Gherbal Youcef";
+}
