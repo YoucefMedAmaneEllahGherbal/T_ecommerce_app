805bb00de42143998e634b7d58ca47fa0d12dcab
YoucefMebAmaneEllahGherbal <gherbalyoucef@gmail.com>
2026-06-18 11:35:04 +0100

    added colors and some logos (app logo and sign in needed logos)

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:t_store_app/utils/theme/theme.dart';
 import 'app.dart';
 
+
 void main() {
+
+  
   runApp(const App());
 }
---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,7 @@
+class Timages {
+  static const String darkAppLogo = "assets/logos/darkmode_icon.png";
+  static const String lightAppLogo = "assets/logos/lihgtmode_icon.png";
+
+  static const String google = "assets/logos/google.png";
+  static const String facebook = "assets/logos/facebook.png";
+}
